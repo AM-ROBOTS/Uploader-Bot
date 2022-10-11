0ffa79c6e2c0b332fa33e7b27912ede4c919a1ac
AM-ROBOTS <115565191+AM-ROBOTS@users.noreply.github.com>
2022-10-12 02:19:37 +0530

    Add files via upload

---
+++ functions/function00.py
@@ -0,0 +1,33 @@
+
+import aiohttp
+from config import Config
+from requests.utils import requote_uri
+
+API_1337x = "https://api.abouthurub.wtf/1337x?query={}&limit={}"
+API_YTS = "https://api.abouthurub.wtf/yts?query={}&limit={}"
+API_PIRATEBAY = "https://api.abouthurub.wtf/piratebay?query={}&limit={}"
+API_ANIME = "https://api.abouthurub.wtf/anime?query={}&limit={}"
+
+
+async def Search1337x(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_1337x.format(query, Config.MAX_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchYTS(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_YTS.format(query, Config.MAX_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchPirateBay(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_PIRATEBAY.format(query, Config.MAX_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchAnime(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_ANIME.format(query, Config.MAX_RESULTS))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
